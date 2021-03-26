0b458839186ab4ffffcbe7b7055b472c7c007804
Lfich <51030430+Lfich@users.noreply.github.com>
2021-03-26 12:26:08 +0100

    Mise à jour main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.chestClosed, function (sprite, location) {
+    game.over(true)
+})
 let mySprite = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
@@ -16,5 +19,6 @@ let mySprite = sprites.create(img`
     . . . 3 3 . . . . . . . . . . . 
     . . . . 3 3 . . . . . . . . . . 
     `, SpriteKind.Player)
-controller.moveSprite(mySprite)
+controller.moveSprite(mySprite, 100, 100)
 tiles.setTilemap(tilemap`niveau1`)
+scene.cameraFollowSprite(mySprite)
